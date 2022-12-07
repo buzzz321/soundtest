aa146dd73a3ac8c4ba98105cabc26ebd0bf8b7d5
buzzz321 <anders.olmegmail.com>
2022-12-07 17:36:01 +0100

    Small refactor

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,33 @@
 const std = @import("std");
 const zaudio = @import("zaudio");
 const content_dir = @import("build_options").content_dir;
+const mem = std.mem;
+
+fn simplePlay(allocator: mem.Allocator) !void {
+    zaudio.init(allocator);
+    defer zaudio.deinit();
+
+    const engine = try zaudio.Engine.create(null);
+    defer engine.destroy();
+
+    const music = try engine.createSoundFromFile(
+        content_dir ++ "elimination.ogg",
+        //content_dir ++ "broke.mp3",
+        .{ .flags = .{ .stream = true } },
+    );
+    defer music.destroy();
+
+    try music.start();
+
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
+    var buf: [10]u8 = undefined;
+
+    try stdout.print("A number please: ", .{});
+    _ = try stdin.readUntilDelimiterOrEof(buf[0..], '\n');
+
+    try music.stop();
+}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -11,29 +38,5 @@ pub fn main() !void {
             @panic("leak");
         }
     }
-    zaudio.init(allocator);
-    defer zaudio.deinit();
-
-    const engine = try zaudio.Engine.create(null);
-     defer engine.destroy();
-
-    const music = try engine.createSoundFromFile(
-        content_dir ++ "elimination.ogg",
-        //content_dir ++ "broke.mp3",
-
-        .{ .flags = .{ .stream = true } },
-    );
-    defer music.destroy();
-
-    try music.start();
-
-    const stdin = std.io.getStdIn().reader();
-    const stdout = std.io.getStdOut().writer();
-    var buf: [10]u8 = undefined;
-    
-    try stdout.print("A number please: ", .{});
-    _=try stdin.readUntilDelimiterOrEof(buf[0..], '\n');
-
-    try music.stop();
-   
+    try simplePlay(allocator);
 }
